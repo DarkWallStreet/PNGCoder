af9d43d3a8d50e1bc7c945dc4706dc8ddf0e8a3e
DarkWallStreet <Harry.vakhlakov@yandex.ru>
2025-03-28 21:12:34 +0300

    0.1.1

--- key_creator.py
+++ key_creator.py
@@ -18,7 +18,8 @@ def key_creator(file_name: str = 'key'):
     global alp
     variants = []
     i = 0
-    while i != ((len(alp) * 15) + 9):
+    f = randint(10, 20)
+    while i != ((len(alp) * f) + 9):
         n = randint(0, 255), randint(0, 255), randint(0, 255)
         if n not in variants and n != (0,0,0):
             variants.append(n)
@@ -26,7 +27,7 @@ def key_creator(file_name: str = 'key'):
     dict = {space:variants[:5]}
     variants = variants[5:]
     for item in alp:
-        z = randint(2, 15)
+        z = randint(3, f)
         dict[item] = variants[:z]
         variants = variants[z:]
     dict['unknown'] = variants[:2]
